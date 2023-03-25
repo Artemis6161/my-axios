dc9beff6dd04e7a40140f2d28f943cf6a5fe33dc
sangeetha <Artemis6161@github.com>
2023-03-25 18:44:48 +0530

    basename

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
     <div className='app'>
      
       
-      <RouterProvider  router={router} />
+      <RouterProvider basename="/my-axios" router={router} />
     </div>
   )
 }
